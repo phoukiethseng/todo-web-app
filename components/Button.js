8f42f7284123b832a7f921a5a12d0eb571e265b5
Phou Kiethseng <Kiethseng@Phous-MacBook-Pro.local>
2023-03-04 03:21:01 +0800

    Make homepage looks nicer

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ export default function Button({ onClick, children }) {
   return (
     <button
       onClick={onClick}
-      className="border-2 border-[#00C071] bg-[#00C071] hover:bg-[#005261] rounded-xl px-4 py-3 font-bold text-white col-span-1 drop-shadow-md"
+      className="border-2 border-[#00C071] hover:border-[#005261] bg-[#00C071] hover:bg-[#005261] rounded-xl px-4 py-3 font-bold text-white col-span-1 drop-shadow-md duration-100"
     >
       {children}
     </button>
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
+import Button from "@/components/Button";
 import LoadingPage from "@/components/loading/LoadingPage";
-import NavButton from "@/components/NavButton";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function HomePage() {
@@ -14,27 +15,27 @@ export default function HomePage() {
     );
   return (
     <>
-      {session && (
-        <>
-          <div className="w-full flex flex-col justify-center items-center space-y-6">
-            <p className="text-2xl font-bold">Welcome {session?.user?.name}!</p>
-            <NavButton
-              action={() => {
-                router.push("/todos");
-              }}
-            >
-              View Todos
-            </NavButton>
-          </div>
-        </>
-      )}
+      <div className="w-full min-h-[150px] flex flex-col justify-center items-center space-y-6">
+        {session && (
+          <>
+            <p className="text-2xl font-bold text-slate-700">
+              Welcome {session?.user?.name}!
+            </p>
+            <Button onClick={() => router.push("/todos")}>View Todos</Button>
+          </>
+        )}
 
-      {!session && (
-        <>
-          <p>You have not sign in</p>
-          <a href="api/auth/signin">Sign in</a>
-        </>
-      )}
+        {!session && (
+          <>
+            <p className="text-2xl font-bold text-slate-700">
+              You have not sign in
+            </p>
+            <Link href="/api/auth/signin">
+              <Button>Sign in</Button>
+            </Link>
+          </>
+        )}
+      </div>
     </>
   );
 }
