53767a31ee8a458a78a1df5d48cd6c8ae8e10398
Phou Kiethseng <puseng123@gmail.com>
2023-07-09 17:43:47 +0800

    Avoid next-auth bug after package update  at https://github.com/nextauthjs/next-auth/issues/7166

--- utils/backend/verifyCsrfToken.js
+++ utils/backend/verifyCsrfToken.js
@@ -1,7 +1,7 @@
 import { getCsrfToken } from "next-auth/react";
 
 async function verifyCsrfToken({ csrfToken, req }) {
-  const serverCsrfToken = await getCsrfToken({ req });
+  const serverCsrfToken = await getCsrfToken({ req: { headers: req.headers } });
   return csrfToken && csrfToken === serverCsrfToken;
 }
 
